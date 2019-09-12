19fa7cd9e23f1e7df4fdfde0477956442ba3c3ca
Dmitrii <zeionara@niuitmo.ru>
2019-09-12 20:37:59 +0300

    Fixed test scripts and report for slots-intents

--- scripts/slots-intents/auto-test.py
+++ scripts/slots-intents/auto-test.py
@@ -14,21 +14,13 @@ with open(TEST_FILE) as f:
 counter = 0
 positive_counter = 0
 
-
 # load in pretrained model & corresponding vocab mappings
 loaded_model = BiLSTM_CRF()
 loaded_model.load(MODEL)
 with open(DATASET_INFO,'rb') as f:
    loaded_info = pickle.load(f)
 
-def extract_slots_intents(message):
-	intent, entities = intent_entities(message,loaded_model,loaded_info)
-	es = {"WORDS":[],"SLOTS (ENTITIES)":[]}
-	for word, entity in entities:
-		es["WORDS"].append(word)
-		es["SLOTS (ENTITIES)"].append(entity)
-	return es
-
+print(f"{'Sample':80s}\t{'recognized-label':20s}\t{'true-label':20s}")
 for label in test_dataset.keys():
 	for sample in test_dataset[label]:
 		intent, entities = intent_entities(sample, loaded_model, loaded_info)
