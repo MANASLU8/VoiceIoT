67fda0d64fc1db07a7aa635df9d637530f70afb8
Dmitrii <zeionara@niuitmo.ru>
2019-09-12 15:03:35 +0300

    removed redundant comments from the converting script

--- scripts/snips-nlu/annotations2snips-nlu.py
+++ scripts/snips-nlu/annotations2snips-nlu.py
@@ -16,7 +16,6 @@ def join_empty(collection):
 	res = []
 	for i in range(len(collection)):
 		if 'slot_name' in collection[i] and empty_seq_start_i >= 0 and i - empty_seq_start_i > 1:
-			#res.append({"text": ' '.join([item['text'] for item in collection[empty_seq_start_i: i]])})
 			res.append(join(collection, empty_seq_start_i, i))
 			empty_seq_start_i = -1
 			res.append(collection[i])
@@ -55,68 +54,28 @@ def make_chunk(i, entities, entity_names, word, ontology_label):
 	return {'text': word}
 
 def handle_files(input_files):
-	print(len(input_files))
 	dicti = {}
 	entity_names = set({})
 	for j in range(len(input_files)):
 		input_file = input_files[j]
-		#print('in: ',input_file)
 		with open(input_file) as f:
 			annotation = json.loads(f.read())
-		#print(annotation)
 		ontology_label = extract_ontology_label(annotation)
 		res = []
-		print(re.sub(r'[^\w\s]','',annotation['text'][0]).lower(),'\t',ontology_label)
 		for (i, word) in enumerate(re.sub(r'[^\w\s]','',annotation['text'][0]).lower().split(' ')):
-
 			appended = False
-			# search index in slots-indices
 			if 'slots-indices' in annotation:
-				# for entity_name in annotation['slots-indices'][0].keys():
-				# 	if i in annotation['slots-indices'][0][entity_name]:
-				# 		res.append({'text': word, 'entity': entity_name.strip(), 'slot_name': f"{ontology_label.strip()}.{entity_name.strip()}"})
-				# 		entity_names.add(entity_name.strip())
-				# 		appended = True
-				# 		break
-				# else:
-				# 	res.append({'text': word})
-				# 	appended = True
 				res.append(make_chunk(i, annotation['slots-indices'][0], entity_names, word, ontology_label))
 			elif 'slots-indices-bio' in annotation:
-				# for entity in annotation['slots-indices-bio']:
-				# 	if i in annotation['slots-indices'][0][entity_name]:
-				# 		res.append({'text': word, 'entity': entity_name.strip(), 'slot_name': f"{ontology_label.strip()}.{entity_name.strip()}"})
-				# 		entity_names.add(entity_name.strip())
-				# 		appended = True
-				# 		break
-				# else:
-				# 	res.append({'text': word})
-				# 	appended = True
 				res.append(make_chunk(i, decode_bio(annotation['slots-indices-bio']), entity_names, word, ontology_label))
-
 		res = join_empty(res)
-
 		if ontology_label in dicti:
 			dicti[ontology_label]["utterances"].append({"data": res})
 		else:
 			dicti[ontology_label] = {"utterances": [{"data": res}]}
 	return dicti, entity_names
 
-
-#utterances = [{"data": handle_file(os.path.join(INPUT_FOLDER, filename))} for filename in os.listdir(INPUT_FOLDER)]
-
 with open(OUTPUT_FILE, "w") as f:
-	intents, entities = handle_files(list_dataset_files(INPUT_FOLDER))#[os.path.join(INPUT_FOLDER, filename) for filename in os.listdir(INPUT_FOLDER)])
+	intents, entities = handle_files(list_dataset_files(INPUT_FOLDER))
 	sample_entity = {"data": [], "use_synonyms": True, "automatically_extensible": True, "matching_strictness": 1.0}
 	f.write(json.dumps({"intents": intents, "entities": {entity: sample_entity for entity in entities}, "language": "en"}, indent=2).encode().decode('unicode-escape'))
-
-
-# print(decode_bio([
-# 		[0,"B-COMMAND"],
-# 		[1,"B-DEVICE"],
-# 		[2,"B-Actor"],
-# 		[3,"B-Action"],
-# 		[4,"I-Action"],
-# 		[5, "B-DeviceParameterValue"]
-				
-# 	]))
