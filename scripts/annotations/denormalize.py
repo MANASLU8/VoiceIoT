1956db01e179ce8a8c026f1f457f195665dc98a5
Dmitrii <zeionara@niuitmo.ru>
2019-10-25 15:16:59 +0300

    Updated datasets

--- scripts/annotations/denormalize.py
+++ scripts/annotations/denormalize.py
@@ -23,6 +23,16 @@ def get_flawy(input_files, get_file_names=False):
 			continue
 	return flawy
 
+def get_complex(input_files):
+	for j in range(len(input_files)):
+		input_file = input_files[j]
+		with open(input_file) as f:
+			annotation = json.loads(f.read())
+		if not fe.extract_ontology_label(annotation) or not fe.extract_utterance_type(annotation) or not 'slots-indices' in annotation:
+			print(f"File {input_file} is flawy")
+			flawy[input_file] = annotation
+			continue
+
 def get_two(input_files, get_file_names=False):
 	flawy = {}
 	nice = {}
@@ -30,7 +40,7 @@ def get_two(input_files, get_file_names=False):
 		input_file = input_files[j]
 		with open(input_file) as f:
 			annotation = json.loads(f.read())
-		if not 'slots-indices' in annotation:
+		if not 'slots-indices' in annotation or not fe.extract_utterance_type(annotation) or not fe.extract_ontology_label(annotation):
 			print(f"File {input_file} is flawy")
 			flawy[input_file] = annotation
 		else:
